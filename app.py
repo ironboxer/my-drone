cb57b46894073ed0c7b3ed51ea3ff6eb0982f12d
ltt <ironboxer@qq.com>
2024-08-16 10:13:54 +0800

    datetime

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+from datetime import datetime
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '<h1>Index</h1>'
+    return f'<h1>{datetime.now()}</h1>'
 
 
